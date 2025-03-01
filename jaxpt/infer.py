00bd278f63c67ffcc6f80f643fe5e7a809384348
Vikram  Pawar <128307105+novastar53@users.noreply.github.com>
2025-02-28 23:32:25 -0500

    Implemented gradient accumulation  (#3)
    * Added a GPU performance tuning notebook
    
    * Added pytorch performance tuning notebook
    
    * Fixed bug in forward pass
    
    * Moved dataloader into jitted function. Didn't have much of an impact.
    
    * Implemented gradient clipping
    
    * Added warmup + cosine lr schedule
    
    * Removed jit from generate function as it slows down inference
    
    * Tested OpenAI's Triton framework
    
    * Implemented gradient accumulation to increase effective batch size

--- jaxpt/infer.py
+++ jaxpt/infer.py
@@ -1,8 +1,9 @@
+from functools import partial
+
 import jax
 import jax.numpy as jnp
 import flax.nnx as nnx
 
-import tiktoken
 
 def top_k_sampling(logits, key, k=50):
     top_k_indices = jnp.argsort(logits, axis=-1)[..., -k:]
@@ -13,32 +14,16 @@ def top_k_sampling(logits, key, k=50):
     return jnp.take_along_axis(top_k_indices, sampled_index[..., None], axis=-1).squeeze(-1), key
 
 
-@nnx.jit
-def _generate(model, x):
-    return model(x)
+#@nnx.jit(static_argnames=("max_length", "temperature", "top_k"))
+def generate(model: nnx.Module,  *, x: jax.Array, max_length=50,
+                        temperature=0.7, top_k = 50) -> jax.Array:
 
+    key = jax.random.PRNGKey(0)
 
-def generate_completion(model: nnx.Module, prefix: str, *, max_length=50, num_completions=5, 
-                        temperature=0.7, top_k = 50):
-
-    key = jax.random.PRNGKey(0)    
-    model.eval()
-    enc = tiktoken.get_encoding('gpt2')
-    tokens = enc.encode(prefix)
-    tokens = jnp.array(tokens, dtype=jnp.int32) 
-
-    tokens = jnp.expand_dims(tokens, axis=0)
-    x = jnp.tile(tokens, (num_completions, 1)) 
-
-    while x.shape[1] < max_length: 
-        logits = _generate(model, x) 
-        logits = logits[:, -1, :] / temperature # (B, vocab_size)
+    while x.shape[1] < max_length:
+        logits = model(x)[:, -1, :] / temperature
         x_next, key = top_k_sampling(logits, key, k=top_k)
         x_next = x_next.reshape(x_next.shape[0], 1)
-        x = jnp.concatenate((x, x_next), axis=1) # (B, T+1)
- 
-    for i in range(num_completions):
-        tokens = x[i, :max_length].tolist()
-        decoded = enc.decode(tokens)
-        print(">", decoded)
+        x = jnp.concatenate((x, x_next), axis=1) # (B, T+1)#
+    return x
 
--- jaxpt/train.py
+++ jaxpt/train.py
@@ -1,18 +1,41 @@
 import jax
+import jax.numpy as jnp
 from flax import nnx
 import optax
 
 from flax.training import train_state 
 
-@nnx.jit
-def train_step(model, optimizer, batch, targets):
 
-    def loss_fn(model, batch, targets):
-        logits = model(batch)
-        loss = optax.softmax_cross_entropy_with_integer_labels(logits, targets).mean()
-        return loss
-  
-    loss, grads =  nnx.value_and_grad(loss_fn)(model, batch, targets)
-    optimizer.update(grads)
+def compute_global_norm(grads):
+    return jnp.sqrt(sum(jnp.sum(g**2) for g in jax.tree_util.tree_leaves(grads)))
+
+
+def loss_fn(model, batch, targets):
+    logits = model(batch)
+    loss = optax.softmax_cross_entropy_with_integer_labels(logits, targets).mean()
     return loss
+
+
+
+@nnx.jit(static_argnames=("B", "T"))
+def accum_step(model, data, B, T, accum_grad, accum_loss, rng):
+    k = jax.random.randint(rng, (1,), 0, len(data) - B*T - 1)[0]
+    batch = jax.lax.dynamic_slice(data, (k,), (B*T,)).reshape((B, T))
+    targets = jax.lax.dynamic_slice(data, (k+1,), (B*T,)).reshape((B, T))
+    loss, grads =  nnx.value_and_grad(loss_fn)(model, batch, targets)
+    accum_grad = jax.tree_util.tree_map(lambda x, y: x + y, accum_grad, grads)
+    accum_loss = accum_loss + loss
+    return accum_grad, accum_loss
+
+
+@nnx.jit(static_argnames=("B", "T"))
+def train_step(model, optimizer, data, B, T, rng):
+    k = jax.random.randint(rng, (1,), 0, len(data) - B*T - 1)[0]
+    batch = jax.lax.dynamic_slice(data, (k,), (B*T,)).reshape((B, T))
+    targets = jax.lax.dynamic_slice(data, (k+1,), (B*T,)).reshape((B, T))
+    loss, grads =  nnx.value_and_grad(loss_fn)(model, batch, targets)
+    norm = compute_global_norm(grads)
+    optimizer.update(grads)
+    return loss, norm
+
 
