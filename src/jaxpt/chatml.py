c65886081018d904d00eb5897b78428b5499d64c
Vikram Pawar <pvikram035@gmail.com>
2025-05-21 03:10:03 -0400

    Upgraded chat for ChatML style prompts.

---
+++ src/jaxpt/chatml.py
@@ -0,0 +1,25 @@
+import logging
+
+import numpy as np
+
+
+logger = logging.getLogger(__name__)
+
+
+def format_as_gpt4_chatml_and_tokenize(tokenizer,
+                            question="What is photosynthesis?",
+                            system_prompt="You are a helpful assistant.",
+                            debug=False,
+                            logger=logger):
+    
+    x = [
+        "<|im_start|>system\n" + system_prompt + "<|im_end|>\n",
+        "<|im_start|>user\n" + question + "<|im_end|>\n",
+        "<|im_start|>assistant\n" 
+        ] 
+    x = "".join(x)
+    logger.debug(f"Formatted input:\n{x}")
+    tok_x = np.array(tokenizer.encode(x), dtype=np.uint16)
+    logger.debug(f"Tokenized input of length {len(tok_x)} generated.")
+    return tok_x
+ 
