04f384ebbf4fff113878eddb0c1afe4638a6c0f7
Vikram Pawar <pvikram035@gmail.com>
2025-07-21 12:21:24 -0400

    Updated GLU_GPT model and train notebook.

--- src/jaxpt/models/glu_gpt.py
+++ src/jaxpt/models/glu_gpt.py
@@ -1,35 +1,69 @@
 from typing import Literal, Optional
 from dataclasses import dataclass
 
+import jax
 import jax.numpy as jnp
 import flax.nnx as nnx
 
 from jaxpt.modules.attention import CausalSelfAttention
 from jaxpt.modules.mlp import GLU
 from jaxpt.modules.config import Config
+from jaxpt.utils import update_param, get_param
 
+from transformers import GPT2LMHeadModel
 import orbax.checkpoint as ocp
 
 
-@dataclass
-class GLUGPTConfig(Config):
+@dataclass(eq=True, unsafe_hash=True)
+class GLU_GPTConfig(Config):
     dtype: jnp.dtype = jnp.float32
     block_size: int = 1024  # sequence length
     vocab_size: int = 50304  # 50257 padded to the nearest multiple of 64
     n_layer: int = 12  # number of attention blocks
     n_head: int = 12  # number of attention heads
-    n_embed: int = 768  # number token embedding dimensionsa
-    n_mlp_hidden: int = 4 * 768  # hiden size for piecewise FFN
+    n_embed: int = 768  # number token embedding dimensions
+    n_mlp_hidden: int = 4 * 768  # hidden size for piecewise FFN
+    mlp_bias: bool = False # MLP/GLU layer bias toggle
     ln_epsilon: float = 1e-5
-    sdpa_implementation: Literal["xla", "cudnn"] = "xla"
+    init_stddev: float = 0.02  # stddev for layer init
+    sdpa_implementation: Literal["xla", "cudnn"] = ("xla")
+    glu_activation: Literal["gelu", "silu", "sigmoid"] = (
+        "silu"  # glu activation or gating function
+    )
+
+    #mesh: jax.sharding.Mesh | None = None  # device mesh
+    
+    embed_partition_spec: tuple = (None,)
+    pos_embed_partition_spec: tuple = (None,)
+    ln_partition_spec: tuple = (None,)
+
+    glu_fc_kernel_sharding: tuple = (None,)
+    glu_fc_bias_sharding: tuple = (None,)
+    glu_gate_kernel_sharding: tuple = (None,)
+    glu_gate_bias_sharding: tuple = (None,)
+    glu_proj_kernel_sharding: tuple = (None,)
+    glu_proj_bias_sharding: tuple = (None,)
+
+    attn_c_attn_kernel_sharding: tuple = (None,)
+    attn_c_attn_bias_sharding: tuple = (None,)
+    attn_c_proj_kernel_sharding: tuple = (None,)
+    attn_c_proj_bias_sharding: tuple = (None,)
 
 
 class Block(nnx.Module):
-    def __init__(self, config: GLUGPTConfig, rngs: nnx.Rngs):
+    def __init__(self, config: GLU_GPTConfig, rngs: nnx.Rngs):
         self.ln_1 = nnx.LayerNorm(
             config.n_embed,
             epsilon=config.ln_epsilon,
             dtype=config.dtype,
+            scale_init=nnx.with_partitioning(
+                nnx.initializers.ones,
+                getattr(config, "ln_partition_spec", (None,))
+            ),
+            bias_init=nnx.with_partitioning(
+                nnx.initializers.zeros,
+                getattr(config, "ln_partition_spec", (None,))
+            ),
             rngs=rngs,
         )
         self.attn = CausalSelfAttention(config, rngs=rngs)
@@ -37,23 +71,34 @@ class Block(nnx.Module):
             config.n_embed,
             epsilon=config.ln_epsilon,
             dtype=config.dtype,
+            scale_init=nnx.with_partitioning(
+                nnx.initializers.ones,
+                getattr(config, "ln_partition_spec", (None,))
+            ),
+            bias_init=nnx.with_partitioning(
+                nnx.initializers.zeros,
+                getattr(config, "ln_partition_spec", (None,))
+            ),
             rngs=rngs,
         )
-        self.mlp = GLU(config, rngs=rngs)
+        self.glu = GLU(config, rngs=rngs)
 
     def __call__(self, x):
         x = self.attn(self.ln_1(x)) + x
-        x = self.mlp(self.ln_2(x)) + x
+        x = self.glu(self.ln_2(x)) + x
         return x
 
 
-class GLUGPT(nnx.Module):
-    def __init__(self, config: GLUGPTConfig, rngs: nnx.Rngs):
+class GLU_GPT(nnx.Module):
+    def __init__(self, config: GLU_GPTConfig, rngs: nnx.Rngs):
         self.config = config
         self.wte = nnx.Embed(
             config.vocab_size,
             config.n_embed,
-            embedding_init=nnx.initializers.normal(stddev=0.02),
+            embedding_init=nnx.with_partitioning(
+                nnx.initializers.normal(stddev=config.init_stddev, dtype=config.param_dtype),
+                getattr(config, "embed_partition_spec", (None,))
+            ),
             dtype=config.dtype,
             param_dtype=config.dtype,
             rngs=rngs,
@@ -61,7 +106,10 @@ class GLUGPT(nnx.Module):
         self.wpe = nnx.Embed(
             config.block_size,
             config.n_embed,
-            embedding_init=nnx.initializers.normal(stddev=0.02),
+            embedding_init=nnx.with_partitioning(
+                nnx.initializers.normal(stddev=config.init_stddev, dtype=config.param_dtype),
+                getattr(config, "pos_embed_partition_spec", (None,))
+            ),
             dtype=config.dtype,
             rngs=rngs,
         )
@@ -70,14 +118,22 @@ class GLUGPT(nnx.Module):
             config.n_embed,
             epsilon=config.ln_epsilon,
             dtype=config.dtype,
+            scale_init=nnx.with_partitioning(
+                nnx.initializers.ones,
+                getattr(config, "ln_partition_spec", (None,))
+            ),
+            bias_init=nnx.with_partitioning(
+                nnx.initializers.zeros,
+                getattr(config, "ln_partition_spec", (None,))
+            ),
             rngs=rngs,
         )
 
     def __call__(self, idx):
         T = idx.shape[1]
         pos = jnp.arange(0, T, dtype=jnp.uint16)
-        pos_emb = self.wpe(pos)
-        tok_emb = self.wte(idx)
+        pos_emb = self.wpe(pos)  # (T x C)
+        tok_emb = self.wte(idx)  # (B x T x C)
         x = tok_emb + pos_emb
         for block in self.h:
             x = block(x)
@@ -91,11 +147,9 @@ class GLUGPT(nnx.Module):
         ckptr.save(fpath, other_state)
 
     @staticmethod
-    def from_checkpoint(
-        fpath: str, rngs: nnx.Rngs, config=Optional[GLUGPTConfig]
-    ):
-        config = config if config else GLUGPTConfig()
-        model = GLUGPT(config=config, rngs=rngs)
+    def from_checkpoint(fpath: str, rngs: nnx.Rngs, config=Optional[GLU_GPTConfig]):
+        config = config if config else GLU_GPTConfig()
+        model = GPT(config=config, rngs=rngs)
         _, _, other_state = nnx.split(model, nnx.RngState, ...)
         checkpointer = ocp.StandardCheckpointer()
         other_state = checkpointer.restore(fpath, target=other_state)
