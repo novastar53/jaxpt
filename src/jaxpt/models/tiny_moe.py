d22365d78a86f7a5efd208cddee2adc4dc14208b
Vikram Pawar <pvikram035@gmail.com>
2025-07-26 23:17:25 +0000

    Attempted to train tiny moe on a larger dataset. The loss is suspiciously low. Need to investigate.

--- src/jaxpt/models/tiny_moe.py
+++ src/jaxpt/models/tiny_moe.py
@@ -32,8 +32,8 @@ class Tiny_MoE_Config(Config):
     n_experts: int = 8  # number of experts
     mesh: jax.sharding.Mesh = None  # device mesh
     top_k: int = 2  # number of top experts to use
-    load_factor = 1.1 # load factor for expert buffers
-    expert_weight_priority = True # sort expert buffer assignments by expert weight 
+    load_factor: int = 1.1 # load factor for expert buffers
+    expert_weight_priority: bool = True # sort expert buffer assignments by expert weight 
     aux_loss_coeff: float = 1e-2  # moe auxiliary loss coefficient
     n_mlp_hidden: int = 2304  # number of hidden dimensions
     mlp_bias: bool = False  # use bias in mlp layers
