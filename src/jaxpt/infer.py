7b53a14f3c12c8dc1622f135b604e99bdcbacb2a
Vikram Pawar <pvikram035@gmail.com>
2025-08-11 11:59:45 -0400

    Made changes to the inference implementation.

--- src/jaxpt/infer.py
+++ src/jaxpt/infer.py
@@ -13,70 +13,27 @@ from jaxpt.chatml import format_as_gpt4_chatml_and_tokenize
 
 logger = logging.getLogger(__name__)
 
-
-def top_k_sampling(logits, key, k=50):
-    top_k_logits, top_k_indices = jax.lax.top_k(
-        logits, k=k
-    )  # Get the top k logits and indices
-    key, subkey = jax.random.split(key)  # generate a new random key
+@nnx.jit
+def _generate_step(m, x, attn_mask, temperature, key):
+    logits = m(x, mask=attn_mask) / temperature
+    x_new = logits[:, -1, :]
+    top_k_vals, top_k_indices = jax.lax.top_k(x_new, 50)
+    key, subkey = jax.random.split(key)
     top_k_logit_idxs = jax.random.categorical(
-        subkey, top_k_logits
-    )  # sample from the top k logits
+        subkey, top_k_vals
+    )
     top_k_logit_idxs = top_k_logit_idxs[..., None]  # expand dims
     sample_idxs = jnp.take_along_axis(
         top_k_indices, top_k_logit_idxs, axis=-1
-    )  # pick out the sampled tokens
-    sample_idxs = sample_idxs[..., 0]  # squeeze dims
-    return sample_idxs, key
+    )
+    return sample_idxs
 
 
-def generate_slow(
-    model: nnx.Module,
-    *,
-    x: jax.Array,
-    attn_mask: jax.Array | None = None,
-    key: jax.random.PRNGKey,
-    max_length=50,
-    temperature=0.2,
-    top_k=50,
-) -> jax.Array:
-    while x.shape[1] < max_length:
-        logits = model(x, attn_mask) / temperature
-        logits = logits[:, -1, :]
-        x_next, key = top_k_sampling(logits, key, k=top_k)
-        x_next = x_next[..., None]
-        x = jnp.concatenate((x, x_next), axis=1)  # (B, T+1)#
-        attn_mask = jnp.concatenate((attn_mask, jnp.ones((attn_mask.shape[0], 1), dtype=jnp.bool)), axis=1)
-    return x
-
-
-def generate(
-    model: nnx.Module,
-    *,
-    x: jax.Array,
-    attn_mask: jax.Array | None = None,
-    key: jax.random.PRNGKey,
-    max_length=50,
-    temperature=0.2,
-    top_k=50,
-) -> jax.Array:
-    logits = model(x, mask=attn_mask) / temperature
-    logits = logits[:, -1, :]
-    x_next, key = top_k_sampling(logits, key, k=top_k)
-    x_next = x_next[..., None]
-    x = jnp.concatenate([x, x_next], axis=1)  # (B, T+1)#
-
-    while x.shape[1] < max_length:
-        logits = model(x_next) / temperature
-        x_next, key = top_k_sampling(logits, key, k=top_k)
-        x = jnp.concatenate((x, x_next), axis=-1)  # (B, T+1)#
-    return x
-
-
-def generate_completions(
+def generate_completion_slow(
     model,
     enc=None,
     prefix="Hello, I'm a language model,",
+    attn_mask: jax.Array | None = None,
     num_completions=8,
     max_length=20,
     temperature=0.2,
@@ -87,15 +44,47 @@ def generate_completions(
     if key is None:
         key = jax.random.PRNGKey(1337)
 
-    generate_completion = partial(
-        generate_slow, model, key=key, max_length=max_length, temperature=temperature
-    )
     tokens = enc.encode(prefix)
     tokens = jnp.array(tokens, dtype=jnp.int32)
     tokens = jnp.expand_dims(tokens, axis=0)
     x = jnp.tile(tokens, (num_completions, 1))
+    while x.shape[1] < max_length:
+        x_next = _generate_step(model, x, attn_mask, temperature, key)
+        x = jnp.concatenate((x, x_next), axis=1)  # (B, T+1)#
+        if attn_mask:
+            attn_mask = jnp.concatenate((attn_mask, 
+                                        jnp.ones((attn_mask.shape[0], 1), dtype=jnp.bool)), axis=1)
+    output = []
+    for i in range(num_completions):
+        tokens = x[i, :max_length].tolist()
+        decoded = enc.decode(tokens)
+        output.append(decoded)
+    return output
 
-    x = generate_completion(x=x)  # Make sure you can do a forward pass
+
+def generate_completion_fast(
+    model,
+    enc=None,
+    prefix="Hello, I'm a language model,",
+    attn_mask: jax.Array | None = None,
+    num_completions=8,
+    max_length=20,
+    temperature=0.2,
+    key=None,
+):
+    if enc is None:
+        enc = tiktoken.get_encoding("gpt2")
+    if key is None:
+        key = jax.random.PRNGKey(1337)
+
+    tokens = enc.encode(prefix)
+    tokens = jnp.array(tokens, dtype=jnp.int32)
+    tokens = jnp.expand_dims(tokens, axis=0)
+    x = jnp.tile(tokens, (num_completions, 1))
+    #attn_mask = jnp.ones((attn_mask.shape[0], 1), dtype=jnp.bool)
+    while x.shape[1] < max_length:
+        x_next = _generate_step(model, x, attn_mask, temperature, key)
+        x = x_next
     output = []
     for i in range(num_completions):
         tokens = x[i, :max_length].tolist()
