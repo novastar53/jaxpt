6a15732a2fb5cb3890c21a97d9a656152b943e26
Vikram Pawar <pvikram035@gmail.com>
2025-08-21 08:52:56 -0400

    Updated panchatantra-ryder dataset.

--- datasets/panchatantra-ryder/panchatantra-ryder.py
+++ datasets/panchatantra-ryder/panchatantra-ryder.py
@@ -7,17 +7,18 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    fpath = os.path.join(Path(), "data", "panchatantra-ryder-clean.txt")
+    cwd = Path("datasets/panchatantra-ryder").absolute()
+    fpath = os.path.join(cwd, "data", "panchatantra-ryder-clean.txt")
     with open(fpath, "r") as f:
         txt = f.read()
         enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode_ordinary(txt)
         eot = enc._special_tokens['<|endoftext|>']
-        tokens = [eot]
-        tokens += enc.encode_ordinary(txt)
+        tokens += [eot]
         tokens = np.array(tokens)
         train_tokens = tokens[:int(0.9*len(tokens))]
         valid_tokens = tokens[int(0.9*len(tokens)):]
-        output_path = os.path.join(Path(), "processed", "panchatantra_train_0.npy")
+        output_path = os.path.join(cwd, "processed", "panchatantra_train_0.npy")
         np.save(output_path, train_tokens)
-        output_path = os.path.join(Path(), "processed", "panchatantra_valid_1.npy")
+        output_path = os.path.join(cwd, "processed", "panchatantra_valid_1.npy")
         np.save(output_path, valid_tokens)
